07e3db3d29f9541083c33471e449e957684e34ef
Thiago Peres <thiago.mourao.peres@gmail.com>
2019-11-21 16:52:07 -0300

    Estlização da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Principal</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-md-runner-2-es1-masculino/44/HZM-2403-044/HZM-2403-044_detalhe1.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-md-runner-2-es1-masculino/44/HZM-2403-044/HZM-2403-044_detalhe1.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-md-runner-2-es1-masculino/44/HZM-2403-044/HZM-2403-044_detalhe1.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-md-runner-2-es1-masculino/44/HZM-2403-044/HZM-2403-044_detalhe1.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-md-runner-2-es1-masculino/44/HZM-2403-044/HZM-2403-044_detalhe1.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-md-runner-2-es1-masculino/44/HZM-2403-044/HZM-2403-044_detalhe1.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
