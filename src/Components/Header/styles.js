9f4a268a5f76c8cf6ab212f5f5c4b18df9c9b9d1
Thiago Peres <thiago.mourao.peres@gmail.com>
2019-11-21 17:24:23 -0300

    Estilização do Carrinho

--- src/Components/Header/styles.js
+++ src/Components/Header/styles.js
@@ -12,7 +12,7 @@ export const Cart = styled(Link)`
   display: flex;
   align-items: center;
   text-decoration: none;
-  transition: opacity 0.2;
+  transition: opacity 0.2s;
 
   &:hover {
     opacity: 0.7;
