ffbfb6b65f3494c29ad92738c0fcc179ae64b84d
Thiago Peres <thiago.mourao.peres@gmail.com>
2019-12-10 18:40:27 -0300

    Listando no Carrinho e Produto Duplicado

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,7 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': ['error', 'never'],
     'react/static-property-placement': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'no-param-reassign': 'off',
   },
 };
